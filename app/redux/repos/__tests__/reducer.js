de6a26988af0d464b2f68ef81748c23f6d1b5b31
thegrinder <paul.szczepkowski@gmail.com>
2018-12-22 22:07:40 +0100

    Refactor and test fetchRepos reducer

---
+++ app/redux/repos/__tests__/reducer.js
@@ -0,0 +1,18 @@
+import reposReducer, { initialReposState } from '../reducer';
+import { fetchReposActions } from '../fetchRepos';
+
+describe('fetchRepos reducer', () => {
+  it('return initial state', () => {
+    expect(reposReducer(initialReposState, { type: 'NOOP' })).toEqual(initialReposState);
+  });
+
+  it('handles SUCCEEDED action properly', () => {
+    const data = { 1: 'test' };
+    const expectedState = {
+      ...initialReposState,
+      data,
+    };
+    expect(reposReducer(initialReposState, fetchReposActions.succeeded(data)))
+      .toEqual(expectedState);
+  });
+});
--- app/redux/repos/reducer.js
+++ app/redux/repos/reducer.js
@@ -1,28 +1,23 @@
 import { initialRequestState } from '../helpers';
 import { fetchReposActionTypes, fetchReposReducer } from './fetchRepos';
 
-const initialReposState = {
+export const initialReposState = {
   data: [],
   fetchRepos: initialRequestState,
 };
 
 const reposReducer = (state = initialReposState, action) => {
   switch (action.type) {
-    case fetchReposActionTypes.FAILED:
-    case fetchReposActionTypes.FULFILLED:
-    case fetchReposActionTypes.LOADING:
-    case fetchReposActionTypes.RESET:
-      return {
-        ...state,
-        fetchRepos: fetchReposReducer(state.fetchRepos, action),
-      };
     case fetchReposActionTypes.SUCCEEDED:
       return {
         ...state,
         data: action.data,
       };
     default:
-      return state;
+      return {
+        ...state,
+        fetchRepos: fetchReposReducer(state.fetchRepos, action),
+      };
   }
 };
 
