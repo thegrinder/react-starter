832c348f3ca7a1dbf496a7c36828b97e4ce925a4
thegrinder <paul.szczepkowski@gmail.com>
2019-03-02 17:32:46 +0100

    chore(GlobalStyles): remove unnecessary global styles

--- app/components/GlobalStyles/index.js
+++ app/components/GlobalStyles/index.js
@@ -5,24 +5,6 @@ const GlobalStyles = createGlobalStyle`
     height: 100%;
     width: 100%;
   }
-  body {
-    font-family: Lato, sans-serif;
-    font-weight: 400;
-  }
-  button {
-    font-family: Lato, sans-serif;
-    font-weight: 400;
-    letter-spacing: 1px;
-  }
-  input, textarea, select {
-    font-family: Lato, sans-serif;
-    font-weight: 300;
-    font-size: 16px;
-  }
-  h1, h2, h3, h4, h5, h6 {
-    font-family: Lato, sans-serif;
-    font-weight: 300;
-  }
 `;
 
 export default GlobalStyles;
