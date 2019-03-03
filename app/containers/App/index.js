e56a278f7825eb91d8bfac2d700656f85c56b19c
thegrinder <paul.szczepkowski@gmail.com>
2019-03-03 19:05:26 +0100

    refactor(App): refactor to functional component

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { HashRouter, Route } from 'react-router-dom';
@@ -13,20 +13,16 @@ const store = configureStore();
 
 runSaga();
 
-class App extends Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <Provider store={store}>
-        <IntlProvider locale="en">
-          <ThemeProvider theme={theme}>
-            <HashRouter>
-              <Route path="/" component={Main} />
-            </HashRouter>
-          </ThemeProvider>
-        </IntlProvider>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <IntlProvider locale="en">
+      <ThemeProvider theme={theme}>
+        <HashRouter>
+          <Route path="/" component={Main} />
+        </HashRouter>
+      </ThemeProvider>
+    </IntlProvider>
+  </Provider>
+);
 
 export default App;
