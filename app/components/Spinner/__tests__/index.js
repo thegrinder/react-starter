6f8507a48b3543f46f03856043d269efb21fb212
thegrinder <paul.szczepkowski@gmail.com>
2019-04-14 15:25:37 +0200

    test(Spinner): test the component

---
+++ app/components/Spinner/__tests__/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render, cleanup } from 'react-testing-library';
+
+import theme from '../../../theme/components';
+import Spinner from '..';
+
+const requiredProps = {
+  color: 'black',
+};
+
+const renderComponent = (props = {}) => render(
+  <ThemeProvider theme={theme}>
+    <Spinner {...requiredProps} {...props} />
+  </ThemeProvider>,
+);
+
+describe('<Spinner />', () => {
+  afterEach(cleanup);
+
+  it('should render correctly with default props', () => {
+    const { container: { firstChild } } = renderComponent();
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render correctly with custom props', () => {
+    const { container: { firstChild } } = renderComponent({ color: 'white', sizing: 40 });
+    expect(firstChild).toBeDefined();
+  });
+
+  it('should render <span> tag', () => {
+    const { container: { firstChild } } = renderComponent();
+    expect(firstChild.tagName).toEqual('SPAN');
+  });
+});
