92110d7a35f5efd61e3ee07fd6fca931e68cbf00
thegrinder <paul.szczepkowski@gmail.com>
2019-04-21 11:58:44 +0200

    feat(Card): create the component

---
+++ app/components/Card/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { getColor } from 'basic-styled-uikit';
+import { rem } from '../../helpers/utils';
+
+const Card = styled.div`
+  background: ${getColor('white')};
+  border-radius: ${rem(6)};
+  box-shadow: 0 1px 3px 0 ${getColor('gray300')},
+    0 0 0 1px ${getColor('gray300')};
+`;
+
+export default Card;
