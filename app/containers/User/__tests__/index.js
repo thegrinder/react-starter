f537f59e6bf91bfe8a0aedba6bcfe3b112cd8934
thegrinder <paul.szczepkowski@gmail.com>
2019-05-01 00:58:53 +0200

    test(User): fix failing tests

--- app/containers/User/__tests__/index.js
+++ app/containers/User/__tests__/index.js
@@ -1,51 +1,65 @@
-// import React from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter, Route } from 'react-router-dom';
 
-// import { render } from '../../../helpers/test-utils';
-// import { User } from '..';
+import { render, createStore } from '../../../helpers/test-utils';
+import { useFetchUserActions } from '../../../redux/users';
+import { User } from '..';
 
-// const id = 'id';
-// const fetchUser = jest.fn();
-// const requiredProps = {
-//   fetchUser,
-//   loading: false,
-//   match: {
-//     params: {
-//       id,
-//     },
-//   },
-//   user: {
-//     name: 'name',
-//     username: 'username',
-//     email: 'email@email.com',
-//     phone: '132456776',
-//   },
-// };
+jest.mock('../../../redux/users');
 
-// const renderComponent = (props = {}) => render(
-//   <User {...requiredProps} {...props} />,
-// );
 
-// describe('<User />', () => {
-//   it('should render correctly', () => {
-//     const { container: { firstChild } } = renderComponent();
-//     expect(firstChild).toBeDefined();
-//     expect(firstChild).toMatchSnapshot();
-//   });
+const id = 'id';
+const state = {
+  users: {
+    data: {
+      [id]: {
+        id,
+        name: 'name',
+        username: 'username',
+        email: 'email@email.com',
+        phone: '132456776',
+      },
+    },
+    requests: {
+      fetchUser: {
+        loading: false,
+      },
+    },
+  },
+};
 
-//   it('should render the spinner if loading flag set to true', () => {
-//     const { container: { firstChild }, getByLabelText } = renderComponent({ loading: true });
-//     const spinnerElement = getByLabelText('loading...');
-//     expect(firstChild).toContainElement(spinnerElement);
-//   });
+const renderComponent = store => render(
+  <MemoryRouter initialEntries={['/users/id']}>
+    <Provider store={store}>
+      <Route path="/users/:id" component={User} />
+    </Provider>
+  </MemoryRouter>,
+);
 
-//   it('should render the spinner if user is not defined', () => {
-//     const { container: { firstChild }, getByLabelText } = renderComponent({ user: undefined });
-//     const spinnerElement = getByLabelText('loading...');
-//     expect(firstChild).toContainElement(spinnerElement);
-//   });
+describe('<User />', () => {
+  it('should render correctly', () => {
+    const store = createStore(state);
+    const { container: { firstChild } } = renderComponent(store);
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toMatchSnapshot();
+  });
 
-//   it('should fetch user with a specific id on mount', () => {
-//     renderComponent();
-//     expect(fetchUser).toHaveBeenCalledWith(id);
-//   });
-// });
+  // it('should render the spinner if loading flag set to true', () => {
+  //   const { container: { firstChild }, getByLabelText } = renderComponent();
+  //   const spinnerElement = getByLabelText('loading...');
+  //   expect(firstChild).toContainElement(spinnerElement);
+  // });
+
+  // it('should render the spinner if user is not defined', () => {
+  //   const { container: { firstChild }, getByLabelText } = renderComponent();
+  //   const spinnerElement = getByLabelText('loading...');
+  //   expect(firstChild).toContainElement(spinnerElement);
+  // });
+
+  it('should fetch user with a specific id on mount', () => {
+    const store = createStore(state);
+    renderComponent(store);
+    expect(useFetchUserActions().fetchUser).toHaveBeenCalledWith(id);
+  });
+});
