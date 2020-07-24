6ef45d073d7a0f9eb4cc7cc829b2efef5a0fd4ef
thegrinder <paul.szczepkowski@gmail.com>
2020-07-24 13:40:16 +0100

    chore: remove react-modal

--- app/index.js
+++ app/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 
 import App from './containers/App';
-
-Modal.setAppElement('#app');
 
 ReactDOM.render(<App />, document.getElementById('app'));
